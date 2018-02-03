2d6dc0021495d15497d66a5548a42e93df74a39b
Saurabh S Bagalkar <sbagalka@asu.edu>
2018-02-03 14:56:59 -0600

    logits and labels

--- main.py
+++ main.py
@@ -112,6 +112,12 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
+    # Define and reshape logits
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+
+    #Define and reshape labels
+    labels = tf.reshape(correct_label, (-1, num_classes))
+    
     return None, None, None
 tests.test_optimize(optimize)
 
