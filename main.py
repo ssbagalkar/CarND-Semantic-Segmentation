b46c8c7cba4c781e6bbdacfd8e7a37d276343e50
Saurabh S Bagalkar <sbagalka@asu.edu>
2018-02-03 17:20:05 -0600

    lowercase

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     """
     # TODO: Implement function
     for ii in range(epochs):
-        print("EPOCH {} ...".format(ii + 1))
+        print("Epoch {} ...".format(ii + 1))
         for image, label in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss],
                                feed_dict={input_image: image, correct_label: label, keep_prob: 0.5,
